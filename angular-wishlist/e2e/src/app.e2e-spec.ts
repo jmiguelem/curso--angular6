12a3e1e573965c81bf49639cc1d47e9a41e18fe4
Miguel <miguel.elizaldem@outlook.com>
2021-01-24 23:54:17 -0600

    Lista de destinos

--- angular-wishlist/e2e/src/app.e2e-spec.ts
+++ angular-wishlist/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('angular-hello app is running!');
+    expect(await page.getTitleText()).toEqual('angular-wishlist app is running!');
   });
 
   afterEach(async () => {
